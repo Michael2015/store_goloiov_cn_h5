43ecdbba2b341a9e9ad11ffa5a4838a53711ddc0
leftjr <1581382597@qq.com>
2019-11-09 17:19:52 +0800

    修改时间戳显示

--- src/components/me/admin/applylist.vue
+++ src/components/me/admin/applylist.vue
@@ -35,31 +35,6 @@ export default {
   methods: {
     checkitem(id) {
       this.$router.push(`/applydetail?id=${id}`);
-    },
-    handleTime(timeer) {
-      Date.prototype.Format = function(fmt) {
-        var o = {
-          "M+": this.getMonth() + 1, // 月份
-          "d+": this.getDate(), // 日
-          "h+": this.getHours(), // 小时
-          "m+": this.getMinutes(), // 分
-          "s+": this.getSeconds(), // 秒
-          "q+": Math.floor((this.getMonth() + 3) / 3), // 季度
-          S: this.getMilliseconds() // 毫秒
-        };
-        if (/(y+)/.test(fmt))
-          fmt = fmt.replace(RegExp.$1, this.getFullYear() + "");
-        for (var k in o)
-          if (new RegExp("(" + k + ")").test(fmt))
-            fmt = fmt.replace(
-              RegExp.$1,
-              RegExp.$1.length == 1
-                ? o[k]
-                : ("00" + o[k]).substr(("" + o[k]).length)
-            );
-        return fmt;
-      };
-      return new Date(timeer).Format("yy-MM-dd hh:mm:ss"); //"2018-11-15 17:40:00"
     }
   },
   mounted() {
@@ -67,10 +42,11 @@ export default {
       res = res.map(item => {
         return {
           ...item,
-          addtime: this.handleTime(item.addtime)
+          addtime: new Date(item.addtime* 1000).toLocaleString()
         };
       });
       this.showList = res;
+      
     });
   }
 };
