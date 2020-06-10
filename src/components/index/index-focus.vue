2a71e3d16441caa808c4f4a588dcf14f3126399e
杨明昭 <13794888292@163.com>
2020-06-10 15:40:20 +0800

    修改图标文字大小

--- src/components/index/index-focus.vue
+++ src/components/index/index-focus.vue
@@ -76,7 +76,7 @@ export default {
       line-height: size(50);
       width: 100%;
       display: inline-block;
-      font-size: size(27); 
+      font-size: size(22); 
     }
   }
   .clearfix:after {
