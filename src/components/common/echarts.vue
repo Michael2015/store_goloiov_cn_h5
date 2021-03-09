b0249b2991a3a4d55701468f071084cc9bf79162
zhouyuefei20 <zhouyuefei20@163.com>
2021-03-10 01:02:05 +0800

    订单恢复

--- src/components/common/echarts.vue
+++ src/components/common/echarts.vue
@@ -8,28 +8,28 @@ let echarts = require('echarts');
 export default {
   components: {},
   props: ['option'],
-  data() {
+  data () {
     return {
 
     };
   },
   watch: {
     option: {
-      handler(val) {
+      handler (val) {
         this.setData(val)
       },
       deep: true
     }
   },
   methods: {
-    setData(val) {
+    setData (val) {
       let element = this.$refs.echarts;
       let myChart = echarts.init(element);
       // 使用刚指定的配置项和数据显示图表。
       myChart.setOption(val);
     }
   },
-  mounted() {
+  mounted () {
 
 
   },
